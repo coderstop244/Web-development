bf59729e188423e1591515c965e15ed527405a8b
coderstop244 <152056461+coderstop244@users.noreply.github.com>
2026-01-22 07:11:35 +0000

    basics

---
+++ .devcontainer/basicsjs/02_datatypes.js
@@ -0,0 +1,20 @@
+// let score ="33aa";
+
+// let stringnumber=Number(score);
+
+// console.log(stringnumber)// NaN(not a number)
+// console.log(typeof(stringnumber))// number
+
+let isboolean=0;
+let decide=Boolean(isboolean);
+console.log(decide)
+
+//0=>false,1=>true
+
+//""=>false,"Mihir"=>true
+
+let number=33;
+
+let define=String(number);
+
+console.log(typeof(define));
